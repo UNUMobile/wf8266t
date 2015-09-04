520a40e352bad1226955e3fe1ac61649d93081aa
UNU Product API <ckenson@gmail.com>
2015-09-04 10:16:18 +0800

    WF8266T DEMO
    Demo code for WF8266T-LED. Show you how to control WF8266T-LED

---
+++ Sample/LED_Number/LED_Number/LED_Number.ino
@@ -0,0 +1,54 @@
+const int clk = 5;
+const int stb = 6;
+const int dio = 7;
+
+void setup() {
+  pinMode(stb, OUTPUT);
+  pinMode(clk, OUTPUT);
+  pinMode(dio, OUTPUT);
+}
+
+void loop() {
+  displayClear(255); // SHOW ALL LED
+  delay(1000);
+
+  //Light 0x88 ~ 0x8F
+  for(int i =0;i<8;i++)
+  {
+    led_end(0x88+i);
+    delay(1000);
+  }
+
+  displayClear(0);   // CLOSE ALL LED
+  delay(1000);
+
+  //Display Full screen
+  for (int i = 0; i < 3; i++)
+  {
+    displayFull(i, 0x8f); // light 0x88 ~ 0x8f 最暗 到 最亮
+    delay(3000);
+  }
+
+  //Display number by index
+  for (int i = 0; i < 7; i++)
+  {
+    displayNumber(i, i);
+    delay(1000);
+    displayClear(0);
+  }
+
+  //Display number 12 3456
+  // _ is space
+  displayNumber("12_3456");
+  delay(5000);
+
+  //Display icon
+  for (int i = 0; i < 10; i++)
+  {
+    displayIcon(i);
+    delay(1000);
+  }
+
+}
+
+
